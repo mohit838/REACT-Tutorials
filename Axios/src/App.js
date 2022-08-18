71b7c1e55ab52e89f2ce7794cf91cb76a3edce0e
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-18 16:50:41 +0600

    Axios Complete Code

--- Axios/src/App.js
+++ Axios/src/App.js
@@ -1,34 +1,42 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "./axios";
 import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [isError, setIsError] = useState("");
+
+  const getApiData = async () => {
+    try {
+      const res = await axios.get("/posts");
+      setData(res.data);
+    } catch (error) {
+      setIsError(error.message);
+    }
+  };
 
   useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((error) => {});
+    getApiData();
   }, []);
 
   return (
-    <div className="mainApp">
-      <div className="grid">
-        {data.map((post) => {
-          const { id, title, body } = post;
+    <>
+      {isError !== "" && <h2>{isError}</h2>}
+      <div className="mainApp">
+        <div className="grid">
+          {data.slice(0, 12).map((post) => {
+            const { id, title, body } = post;
 
-          return (
-            <div key={id} className="cardDesign">
-              <h2>{title.slice(0, 15).toUpperCase()}</h2>
-              <p>{body.slice(0, 100)}</p>
-            </div>
-          );
-        })}
+            return (
+              <div key={id} className="cardDesign">
+                <h2>{title.slice(0, 15).toUpperCase()}</h2>
+                <p>{body.slice(0, 100)}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
