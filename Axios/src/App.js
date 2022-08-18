e51f8fbaab5bdc02804c992371fd6f7d87059379
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-18 15:35:37 +0600

    Fetch api and show data

--- Axios/src/App.js
+++ Axios/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainApp">
+      <div className="grid">
+        {data.map((post) => {
+          const { id, title, body } = post;
+
+          return (
+            <div key={id} className="cardDesign">
+              <h2>{title}</h2>
+              <p>{body}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
